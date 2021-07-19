4063ab5aa8f1c69df9f1463b248826a5aea7c274
IndoorGhoul <hermione1granger121@gmail.com>
2021-07-19 20:54:31 +0600

    clean

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,8 +5,6 @@ import Map from '../components/Map';
 export default function Main(): React.ReactElement {
   return (
     <main className="page">
-      <h2 className="h2 page__content">Карусель 1</h2>
-      <h2 className="h2 page__content">Карусель 2</h2>
       <Instagram />
       <Map />
     </main>
